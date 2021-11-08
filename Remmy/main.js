bcfe7ade529cdc19aaeffa63e96eacb3b9b7d4aa
Maxim <50755616+Giveruup@users.noreply.github.com>
2021-11-08 20:33:24 +0500

    Delete Remmy directory

--- Remmy/main.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-
-  $("#burger__btn").on("click", function(event) {
-      event.preventDefault();
-      $('#nav').toggleClass('active');
-      $('#burger__btn').toggleClass('active');
-  });
-
-});
